e22226c456b07c84352453514006cd4ef94e066e
Stemmer Matjila <stemmermatjila3@gmail.com>
2025-09-12 15:40:18 +0200

    Add files via upload

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,9 +1,12 @@
 #!/usr/bin/node
 
-const arg = process.argv[2];
+const first = process.argv[2];
+const second = process.argv[3];
 
-if (arg === undefined) {
+if (first === undefined) {
   console.log("No argument");
+} else if (second === undefined) {
+  console.log("Argument found");
 } else {
   console.log("Arguments found");
 }
